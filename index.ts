61b8c312e1de449d7b79fd36b484cbdeb683a46f
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-18 10:43:50 +0530

    Connected the database with the application.

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
-import express, {
-  Express,
-  Request,
-  Response,
-} from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 const app: Express = express();
 dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 
 const port = process.env.PORT || 3200;
 
@@ -14,6 +21,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TS Server...');
 });
 
-app.listen(port, () => {
-  console.log('Server started...');
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database connected successfully...');
+
+    app.listen(port, () => {
+      console.log(`Server started at port ${port}...`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error during data source initialization', err);
+  });
