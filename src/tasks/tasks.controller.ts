cbea4c624866281bd460ce2b6d34a24a11e4f9ad
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-19 07:20:35 +0530

    Refactored the get route and tasks controller.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,22 +1,23 @@
+import { Request, Response } from 'express';
 import { instanceToPlain } from 'class-transformer';
 
 import { Task } from './tasks.entity';
 import { AppDataSource } from '../../index';
 
-export class TaskController {
-  constructor(private taskRepository = AppDataSource.getRepository(Task)) {}
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //@ts-ignore
-  public async getAll(): Promise<Task[]> {
+class TaskController {
+  public async getAll(req: Request, res: Response): Promise<Response> {
     let allTasks: Task[];
 
     try {
-      allTasks = await this.taskRepository.find({ order: { date: 'ASC' } });
+      allTasks = await AppDataSource.getRepository(Task).find({
+        order: { date: 'ASC' },
+      });
       allTasks = instanceToPlain(allTasks) as Task[];
-      return allTasks;
-    } catch (error) {
-      console.log(error);
+      return res.status(200).json(allTasks);
+    } catch (_error) {
+      return res.status(500).json({ error: 'Internal server error.' });
     }
   }
 }
+
+export const taskController = new TaskController();
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,16 +1,12 @@
 import { Router, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 
-import { TaskController } from './tasks.controller';
+import { taskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
 
 export const taskRouter: Router = Router();
 
-taskRouter.get('/tasks', async (req: Request, res: Response) => {
-  const taskController = new TaskController();
-  const allTasks = await taskController.getAll();
-  res.status(200).json(allTasks);
-});
+taskRouter.get('/tasks', taskController.getAll);
 
 taskRouter.post(
   '/tasks',
@@ -24,9 +20,5 @@ taskRouter.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    // const taskController = new TaskController();
-    //   const task = await taskController.createPost();
-    //   res.status(200).json(task);
   },
 );
